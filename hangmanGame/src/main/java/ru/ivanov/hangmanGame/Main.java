464b156a445d338d76011fb7903a6503d472fec2
IIvanov57 <ivanov080892@mail.ru>
2024-02-08 12:02:48 +0300

    init develop

--- hangmanGame/src/main/java/ru/ivanov/hangmanGame/Main.java
+++ hangmanGame/src/main/java/ru/ivanov/hangmanGame/Main.java
@@ -1,7 +1,17 @@
 package ru.ivanov.hangmanGame;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while (true){
+            System.out.println("Напишите \"да\" если хотите начать игру или \"нет\" чтобы выйти из игры");
+            Scanner inputDecide = new Scanner(System.in);
+            String result = inputDecide.nextLine();
+            if (result.equalsIgnoreCase("нет")){
+                break;
+            }
+
+        }
     }
 }
