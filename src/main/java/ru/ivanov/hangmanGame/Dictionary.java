ee4003644fb7ad520acaaa8fd87cd96795503e33
IIvanov57 <ivanov080892@mail.ru>
2024-02-08 23:08:08 +0300

    add dictionaty russian nouns

---
+++ src/main/java/ru/ivanov/hangmanGame/Dictionary.java
@@ -0,0 +1,33 @@
+package ru.ivanov.hangmanGame;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class Dictionary {
+  public static ArrayList<String> worlds = new ArrayList<>();
+
+  public static void addWords() {
+    //File file = new File("src\\main\\java\\resources");
+    try (BufferedReader reader = new BufferedReader(new FileReader("src\\main\\java\\resources\\russian_nouns.txt"))) {
+      String wordLine = reader.readLine();
+
+      while (wordLine != null) {
+        System.out.println(wordLine);
+        //проверяем что слово не коротное и добавляем в список
+        if (wordLine.length() > 5) {
+          worlds.add(wordLine);
+        }
+        //читаем следующий слово
+        wordLine = reader.readLine();
+      }
+
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
--- src/main/java/ru/ivanov/hangmanGame/Main.java
+++ src/main/java/ru/ivanov/hangmanGame/Main.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Dictionary.addWords();
+        Dictionary.worlds.get(0);
         while (true){
-            System.out.println("Напишите \"да\" если хотите начать игру или \"нет\" чтобы выйти из игры");
+            System.out.println("Напишите \"да\" если хотите начать новую игру или \"нет\" чтобы выйти из игры");
             Scanner inputDecide = new Scanner(System.in);
             String result = inputDecide.nextLine();
             if (result.equalsIgnoreCase("нет")){
