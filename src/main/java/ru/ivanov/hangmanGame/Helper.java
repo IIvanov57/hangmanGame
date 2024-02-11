e04a3c3a40485b0679412805c1d015e5e3fd3b9d
IIvanov57 <ivanov080892@mail.ru>
2024-02-11 15:08:22 +0300

    fix validation

--- src/main/java/ru/ivanov/hangmanGame/Helper.java
+++ src/main/java/ru/ivanov/hangmanGame/Helper.java
@@ -4,43 +4,48 @@ import java.util.ArrayList;
 
 public class Helper {
 
-  public static void fillingArray (char [] word, ArrayList<Character> characters){
-    for (int i = 0; i < word.length;i++){
-      char j = 95;
-      characters.add(j);
-    }
-  }
-
-  public static String printArray(ArrayList<Character> characters){
-    String arrayChar = "";
-    for (Character i : characters){
-      //System.out.print(i);
-      arrayChar = arrayChar + "" + i;
-    }
-    return arrayChar;
-  }
-
-  public static int findCharAndCounterMistake(char [] word, ArrayList<Character> characters,char charUser){
-    int count =0;
-    for (int i = 0; i< word.length; i++){
-      if (charUser == word[i]){
-        characters.set(i,word[i]);
-        count++;
-      }
-    }
-    return count;
-  }
-  public static Boolean checkResolution(ArrayList<Character> characters){
-    int count = 0;
-    for (Character i : characters){
-      if(i.charValue() == 95){
-        count++;
-      }
+    public static void fillingArray(char[] word, ArrayList<Character> characters) {
+        for (int i = 0; i < word.length; i++) {
+            char j = 95;
+            characters.add(j);
+        }
     }
 
-    if (count == 0){
-      return true;
+    public static String printArray(ArrayList<Character> characters) {
+        String arrayChar = "";
+        for (Character i : characters) {
+            //System.out.print(i);
+            arrayChar = arrayChar + "" + i;
+        }
+        return arrayChar;
     }
-    return false;
-  }
+
+    public static int findCharAndCounterMistake(char[] word, ArrayList<Character> characters, char charUser) {
+        int count = 0;
+        for (int i = 0; i < word.length; i++) {
+            if (charUser == word[i]) {
+                characters.set(i, word[i]);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static Boolean checkResolution(ArrayList<Character> characters) {
+        int count = 0;
+        for (Character i : characters) {
+            if (i.charValue() == 95) {
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static Boolean checkCharInMistakes(ArrayList<Character> characters, char chatUser) {
+        return characters.contains(chatUser);
+    }
 }
--- src/main/java/ru/ivanov/hangmanGame/Main.java
+++ src/main/java/ru/ivanov/hangmanGame/Main.java
@@ -4,60 +4,62 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    //заполняем словарь
-    Dictionary.addWords();
+    public static void main(String[] args) {
+        //заполняем словарь
+        Dictionary.addWords();
 
-    //начало игры
-    while (true) {
-      System.out.println("Напишите \"да\" если хотите начать новую игру или \"нет\" чтобы выйти из игры");
-      Scanner inputDecide = new Scanner(System.in);
-      String result = inputDecide.nextLine();
-      if (result.equalsIgnoreCase("нет")) {
-        break;
-      }
+        //начало игры
+        while (true) {
+            System.out.println("Напишите \"да\" если хотите начать новую игру или \"нет\" чтобы выйти из игры");
+            Scanner inputDecide = new Scanner(System.in);
+            String result = inputDecide.nextLine();
+            if (result.equalsIgnoreCase("нет")) {
+                break;
+            }
 
-      int countMistake = 0;
-      //массив для загаданного слова
-      char[] word;
-      //список из букв, котрые отгадал игрок
-      ArrayList<Character> wordForGamer = new ArrayList<>();
-      //спписок куда будем складывать буквы, которых нет в загаданном слове
-      ArrayList<Character> mistakeChar = new ArrayList<>();
-      //рандомно выбираем солово из словоря
-      String randomWord = Dictionary.randomWord();
-      word = randomWord.toCharArray();
-      Helper.fillingArray(word, wordForGamer);
-      System.out.println("Слово загаданно! ");
-      while (true) {
-        if (Helper.checkResolution(wordForGamer)) {
-          System.out.println("Победа! Слово: " + Helper.printArray(mistakeChar));
-          break;
+            int countMistake = 0;
+            //массив для загаданного слова
+            char[] word;
+            //список из букв, котрые отгадал игрок
+            ArrayList<Character> wordForGamer = new ArrayList<>();
+            //спписок куда будем складывать буквы, которых нет в загаданном слове
+            ArrayList<Character> mistakeChar = new ArrayList<>();
+            //рандомно выбираем солово из словоря
+            String randomWord = Dictionary.randomWord();
+            word = randomWord.toCharArray();
+            Helper.fillingArray(word, wordForGamer);
+            System.out.println("Слово загаданно! ");
+            while (true) {
+                if (Helper.checkResolution(wordForGamer)) {
+                    System.out.println("Победа! Слово: " + Helper.printArray(mistakeChar));
+                    break;
+                }
+                if (countMistake != 7) {
+                    System.out.println("Слово: " + Helper.printArray(wordForGamer));
+                    System.out.println("Ошибки " + "(" + countMistake + ")" + ": " + Helper.printArray(mistakeChar));
+                    System.out.println("Введите букву: ");
+
+                    Scanner scannerInputChar = new Scanner(System.in);
+                    String charInput = scannerInputChar.nextLine();
+                    if (charInput.length() > 1) {
+                        System.out.println("Введите одину букву!");
+                        continue;
+                    }
+                    char charUser = charInput.toLowerCase().charAt(0);
+
+                    if (Helper.findCharAndCounterMistake(word, wordForGamer, charUser) == 0
+                            && !Helper.checkCharInMistakes(mistakeChar, charUser)) {
+
+                        mistakeChar.add(charUser);
+                        countMistake++;
+                    }
+                } else {
+                    System.out.println("Вы проиграли =(");
+                    break;
+                }
+
+            }
+
         }
-        if (countMistake != 7) {
-          System.out.println("Слово: " + Helper.printArray(wordForGamer));
-          System.out.println("Ошибки " + "(" + countMistake + ")" + ": " + Helper.printArray(mistakeChar));
-          System.out.println("Введите букву: ");
-
-          Scanner scannerInputChar = new Scanner(System.in);
-          String charInput = scannerInputChar.nextLine();
-          if (charInput.length() > 1){
-            System.out.println("Введите одину букву!");
-            continue;
-          }
-          char charUser = charInput.charAt(0);
-
-          if (Helper.findCharAndCounterMistake(word, wordForGamer, charUser) == 0) {
-            mistakeChar.add(charUser);
-            countMistake++;
-          }
-        } else {
-          System.out.println("Вы проиграли =(");
-          break;
-        }
-
-      }
-
     }
-  }
 }
